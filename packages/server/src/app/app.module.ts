5207076d35a1e340383da11d4bf7a4888e738a8e
Shaun Pearson <shaun@bigwhitesolutions.com>
2024-02-05 00:30:59 +0000

    change connection string to support cosmos

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -5,28 +5,46 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { SessionGateway } from './gateways/session.gateway';
 import { CacheService } from './services/cache/cache.service';
 import { PeerServerService } from './services/peer-server/peer-server.service';
-import { HttpModule } from '@nestjs/axios'
+import { HttpModule } from '@nestjs/axios';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UserModule } from './modules/user/user.module';
+import { Logger } from '@nestjs/common';
+
+function getDbConnectionString() {
+  const dbConnectionString =
+    'mongodb://' +
+    process.env.MONGO_DB_URL +
+    ':' +
+    process.env.MONGO_DB_PORT +
+    '/' +
+    process.env.MONGO_DB_NAME +
+    '?ssl=true&replicaSet=globaldb';
+
+  Logger.log(`DB Connection string: ${dbConnectionString}`);
+
+  return dbConnectionString;
+}
 
 @Module({
   imports: [
-    CacheModule.register({ttl: (60 * 60 * 1000), max: 500, isGlobal: true}),
-    MongooseModule.forRoot(`${process.env.MONGO_DB_URL}`, {
-      dbName: process.env.MONGO_DB_NAME,
-      user: process.env.MONGO_DB_USER,
-      pass: process.env.MONGO_DB_PASSWORD
+    CacheModule.register({ ttl: 60 * 60 * 1000, max: 500, isGlobal: true }),
+    MongooseModule.forRoot(getDbConnectionString(), {
+      auth: {
+        username: process.env.MONGO_DB_USER,
+        password: process.env.MONGO_DB_PASSWORD,
+      },
+      retryWrites: false,
     }),
     HttpModule,
-    UserModule
+    UserModule,
   ],
   providers: [
     CacheService,
     PeerServerService,
     SessionGateway,
     { provide: APP_INTERCEPTOR, useClass: CacheInterceptor },
-    GoogleStrategy
+    GoogleStrategy,
   ],
-  exports: []
+  exports: [],
 })
 export class AppModule {}
